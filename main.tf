322b8dcbc60f9b86c5210a58cf3b3b98ec22d893
Rachel Sharp <rachel@hashicorp.com>
2021-01-29 21:04:39 -0500

    initial commit

---
+++ main.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.24.1"
+    }
+  }
+  required_version = "~> 0.14"
+}
+
+variable "region" {
+  default = "us-east-2"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+
+
+resource "aws_security_group" "instance" {
+  name = "terraform-learn-state-sg"
+  ingress {
+    from_port   = "8080"
+    to_port     = "8080"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+output "security_group" {
+  value = aws_security_group.instance.id
+
+}
---
+++ new_state/main.tf
@@ -0,0 +1,61 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.24.1"
+    }
+  }
+  required_version = "~> 0.14"
+}
+
+variable "region" {
+  default = "us-east-2"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+data "terraform_remote_state" "root" {
+  backend = "local"
+
+  config = {
+    path = "../terraform.tfstate"
+  }
+}
+
+resource "aws_instance" "example" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [data.terraform_remote_state.root.outputs.security_group]
+  user_data              = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  tags = {
+    Name = "terraform-learn-state-ec2"
+  }
+}
+
+
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP of the web server"
+}
