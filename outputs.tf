e15126ecbf5ec7ea442b34ccae6d50c63488d731
Robin Norwood <rnorwood@hashicorp.com>
2024-01-16 13:31:11 -0600

    Terraform fmt

--- outputs.tf
+++ outputs.tf
@@ -9,11 +9,11 @@ output "public_ip" {
 }
 
 output "aws_region" {
-  value = var.aws_region
+  value       = var.aws_region
   description = "AWS region for all resources"
 }
 
 output "security_group" {
-  value = aws_security_group.sg_8080.id
+  value       = aws_security_group.sg_8080.id
   description = "The security group for the AWS instance"
 }
