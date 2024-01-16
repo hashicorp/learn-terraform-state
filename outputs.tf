65f6edb5828b98a0fd33e20f55f2a605dcd5c2b5
Robin Norwood <robin.norwood@gmail.com>
2024-01-16 13:29:40 -0600

    Update outputs.tf
    Co-authored-by: Brian McClain <brianmmcclain@gmail.com>

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "aws_region" {
   value = var.aws_region
   description = "AWS region for all resources"
 }
+
+output "security_group" {
+  value = aws_security_group.sg_8080.id
+  description = "The security group for the AWS instance"
+}
